33558ac5602913d54d222a2053ab4ef010478184
Clark L Smith <clarklander1983@gmail.com>
2020-02-08 15:28:17 -0600

    this commit follows completion of Part 3 of the series - Templates

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 #Corey Schafer's django tutorial app
 
 using this to practice working with django for webstack
+
+### django
+ctrl-alt-i  
+//will correct indentation.  you may want to try  
+//it with multiple lines selected  
+
+###Notes
+- in html it is acceptable to use 2 or 4 spaces for indentation just be consistent  
+- some other helpful stuff here that I forgot about
--- ShaferBlog/views.py
+++ ShaferBlog/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 #no longer needed cuz we use render
 
+posts = [
+    {
+        'author': 'ClarkS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'ClarkS',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 31, 2018'
+    },
+]
 
 def home(request):
-    return render(request, 'ShaferBlog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'ShaferBlog/home.html', context)
 
 
 def about(request):
-    return render(request, 'ShaferBlog/about.html')
+    return render(request, 'ShaferBlog/about.html', {'title': 'About'})
