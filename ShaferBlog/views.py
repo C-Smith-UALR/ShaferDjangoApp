31e47aed493ef0864a3b7200a088cb272abb8b06
Clark L Smith <clarklander1983@gmail.com>
2020-02-10 18:41:45 -0600

    completed tutorial part 10: creating new blog posts

--- ShaferBlog/views.py
+++ ShaferBlog/views.py
@@ -3,11 +3,62 @@ from .models import Post #we can just use '.' since we are directory-fellows
 #from django.http import HttpResponse
 #no longer needed cuz we use render
 
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'ShaferBlog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    #<app>/<model>_<viewtype>.html  <--name your templates this way
+    template_name = 'ShaferBlog/home.html'
+    context_object_name = 'posts'   #compare this class view to the function view above
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 
 def about(request):
